8ae1fe45cc9eb629019f8c8d1aa98eb6259f31d8
Steven Isbell <steveni_92@msn.com>
2016-08-27 19:13:42 -0600

    styling changes

--- public/app/directives/sidebar/sidebarCtrl.js
+++ public/app/directives/sidebar/sidebarCtrl.js
@@ -1,26 +1,27 @@
 angular.module('streamalong')
-  .controller('sidebarCtrl', ($scope, sidebarSrvc, $state) => {
+    .controller('sidebarCtrl', ($scope, sidebarSrvc, $state) => {
 
-    $scope.logout = () => {
-      sidebarSrvc.logout().then((response) => {
-        swal("Success!", "Logout Successful!", "success");
-        setTimeout(() => {
-          if (response) { $state.go('landing page'); }
-        }, 1500);
-      });
-    };
+        $scope.logout = () => {
+            sidebarSrvc.logout().then((response) => {
+                swal("Success!", "Logout Successful!", "success");
+                setTimeout(() => {
+                    if (response) {
+                        $state.go('landing page');
+                    }
+                }, 1500);
+            });
+        };
 
-    $scope.putManager = (update, id) => {
-      sidebarSrvc.putManager(update, id).then((response) => {
-        $('.CM-modal').hide(300);
-        console.log(response);
-        if (response.status === 200) {
-        swal("Success!", "Your Information Has Been Saved!", "success");
-      }
-      else {
-        swal("Error!", "Hmm...Something Wasn't Right", "error");
-      }
-      });
-    };
+        $scope.putManager = (update, id) => {
+            sidebarSrvc.putManager(update, id).then((response) => {
+                $('.CM-modal').hide(300);
+                console.log(response);
+                if (response.status === 200) {
+                    swal("Success!", "Your Information Has Been Saved!", "success");
+                } else {
+                    swal("Error!", "Hmm...Something Wasn't Right", "error");
+                }
+            });
+        };
 
-  });
+    });
