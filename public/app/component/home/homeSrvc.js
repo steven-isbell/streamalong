3eaadbb17b93e2ad8742433c348b42261e6490f6
Steven Isbell <steveni_92@msn.com>
2016-10-11 09:49:06 -0600

    clean

--- public/app/component/home/homeSrvc.js
+++ public/app/component/home/homeSrvc.js
@@ -1,49 +1,49 @@
 angular.module('streamalong')
-    .service('homeSrvc', function($q, $http) {
+  .service('homeSrvc', function($q, $http) {
 
-        this.getUser = () => {
-            return $http.get('/me');
-        };
+    this.getUser = () => {
+      return $http.get('/me');
+    };
 
-        this.getQuote = () => {
-            return $http({
-                method: 'GET',
-                url: 'http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en'
-            }).then(function(response) {
-                let results = response.data;
-                return results;
-            });
-        };
+    this.getQuote = () => {
+      return $http({
+        method: 'GET',
+        url: 'http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en'
+      }).then(function(response) {
+        let results = response.data;
+        return results;
+      });
+    };
 
-        this.getAPIWeather = (position) => {
-            var lat, lon;
+    this.getAPIWeather = (position) => {
+      var lat, lon;
 
-            lat = position.coords.latitude;
-            lon = position.coords.longitude;
-            return $http({
-                method: 'GET',
-                url: 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=imperial&APPID=8d6d3d96f61b617556cbc73957e7ae65'
-            });
-        };
+      lat = position.coords.latitude;
+      lon = position.coords.longitude;
+      return $http({
+        method: 'GET',
+        url: 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=imperial&APPID=8d6d3d96f61b617556cbc73957e7ae65'
+      });
+    };
 
 
-        this.getLocation = () => {
-            var deferred = $q.defer();
+    this.getLocation = () => {
+      var deferred = $q.defer();
 
-            function success(position) {
-                deferred.resolve(position);
-            }
-            navigator.geolocation.getCurrentPosition(success);
-            return deferred.promise;
-        };
+      function success(position) {
+        deferred.resolve(position);
+      }
+      navigator.geolocation.getCurrentPosition(success);
+      return deferred.promise;
+    };
 
-        this.checkAuth = () => {
-            return $http({
-                method: 'GET',
-                url: '/checkAuth'
-            }).then((response) => {
-                return response;
-            });
-        };
+    this.checkAuth = () => {
+      return $http({
+        method: 'GET',
+        url: '/checkAuth'
+      }).then((response) => {
+        return response;
+      });
+    };
 
-    });
+  });
